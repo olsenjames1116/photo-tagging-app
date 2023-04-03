8d5a538c94e7d7ae899a73adff3dca207878e6dc
James Olsen <olsenjames1116@gmail.com>
2023-04-03 13:16:48 -0400

    Build content component with a page page cover
    The page cover will be used to block user interaction with the webpage
    during the welcome and exit from the app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Content from './components/content/Content';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Content />
       <Footer />
     </div>
   );
---
+++ src/components/content/Content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageCover from './PageCover';
+
+export default function Content() {
+  return (
+    <div className="content">
+      <PageCover />
+    </div>
+  );
+}
