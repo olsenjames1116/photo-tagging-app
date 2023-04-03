575c3a934bab5f5b379e869bf84ca97920a91060
James Olsen <olsenjames1116@gmail.com>
2023-04-03 12:59:35 -0400

    Build footer component with sources and author
    Some placeholder text was added to ensure the components were rendered
    correctly.

---
+++ src/components/footer/Author.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Author() {
+  return <p className="author">Built and designed by...</p>;
+}
---
+++ src/components/footer/Sources.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Sources() {
+  return <p className="sources">Images provided by...</p>;
+}
