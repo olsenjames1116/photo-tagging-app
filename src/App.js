e67a4807c375f427f969a97e337a5114f23b64b2
James Olsen <olsenjames1116@gmail.com>
2023-04-03 12:53:57 -0400

    Build header component with a title component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
-function App() {}
+import React from 'react';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
 
 export default App;
