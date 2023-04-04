67a0cf0f462055d70b271464f6450c6f6c85dc71
James Olsen <olsenjames1116@gmail.com>
2023-04-04 14:47:12 -0400

    Display target characters in header on game start
    When the start button is pressed, the target characters will appear in
    the header for the user to track their progress and refer back to during
    the game.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Title from './Title';
 import { useSelector } from 'react-redux';
+import Targets from '../content/welcome/Targets';
 
 export default function Header() {
   const welcome = useSelector((state) => state.welcome.value);
   const game = useSelector((state) => state.game.value);
 
-  return <div>{welcome && <Title />}</div>;
+  return (
+    <div className="header">
+      {welcome && <Title />}
+      {game && <Targets />}
+    </div>
+  );
 }
