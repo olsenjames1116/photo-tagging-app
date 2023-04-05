8d66fcd7fa12f6c1aac3326abfb8ff8055d4fd97
James Olsen <olsenjames1116@gmail.com>
2023-04-05 18:58:13 -0400

    Remove targeting box when click is outside content
    Whenever the user clicks outside of the content section of the page, the
    previous targeting box from the image is removed.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Author from './Author';
 import Sources from './Sources';
+import { clickOffImage } from '../../redux/imageClicked/imageClickedSlice';
+import { useDispatch } from 'react-redux';
 
 export default function Footer() {
+  const dispatch = useDispatch();
+
   return (
-    <div className="footer">
+    <div onClick={() => dispatch(clickOffImage())} className="footer">
       <Author />
       <Sources />
     </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Title from './Title';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Targets from '../content/welcome/Targets';
+import { clickOffImage } from '../../redux/imageClicked/imageClickedSlice';
 
 export default function Header() {
   const welcome = useSelector((state) => state.welcome.value);
   const game = useSelector((state) => state.game.value);
 
+  const dispatch = useDispatch();
+
   return (
-    <div className="header">
+    <div onClick={() => dispatch(clickOffImage())} className="header">
       {welcome && <Title />}
       {game && <Targets />}
     </div>
