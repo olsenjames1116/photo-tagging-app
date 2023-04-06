666b71311aa044522b241c1ab7e2c691729fbb46
James Olsen <olsenjames1116@gmail.com>
2023-04-06 15:03:52 -0400

    Validate selected coordinates with character name
    After the user selects the name they believe to be in targeting box,
    validation with the back end will commence.

--- src/components/content/game/Game.js
+++ src/components/content/game/Game.js
@@ -10,49 +10,6 @@ import { db } from '../../../firebase';
 export default function Game() {
   const dispatch = useDispatch();
 
-  // function removeCurrentTargetBox() {
-  //   const currentTargetBox = document.querySelector('div.targetBox');
-  //   const currentTargetList = document.querySelector('div.targetList');
-  //   currentTargetBox && currentTargetBox.remove() && currentTargetList.remove();
-  // }
-
-  // function createTargetList() {
-  //   const targetList = document.createElement('div');
-  //   targetList.classList.add('targetList');
-
-  //   Object.assign(targetList.style, {
-  //     position: 'absolute',
-  //     top: '104px',
-  //     left: '-5px',
-  //     border: '5px solid blue',
-  //     height: '100px',
-  //     width: '100px'
-  //   });
-
-  //   const targetBox = document.querySelector('div.targetBox');
-  //   targetBox.appendChild(targetList);
-  // }
-
-  // function createTargetBox(x, y) {
-  //   // removeCurrentTargetBox();
-  //   // return <TargetBox x={x} y={y} />;
-  //   // const targetBox = document.createElement('div');
-  //   // targetBox.classList.add('targetBox');
-  //   // Object.assign(targetBox.style, {
-  //   //   position: 'absolute',
-  //   //   zIndex: '100',
-  //   //   top: `${y - 50}px`,
-  //   //   left: `${x - 50}px`,
-  //   //   width: '100px',
-  //   //   height: '100px',
-  //   //   border: '5px solid black'
-  //   // });
-  //   // document
-  //   //   .querySelector('div.content')
-  //   //   .insertBefore(targetBox, document.querySelector('div.content > img'));
-  //   // createTargetList();
-  // }
-
   function getCoordinates(event) {
     const { left, top } = event.target.getBoundingClientRect();
     const { clientX, clientY } = event;
@@ -61,33 +18,7 @@ export default function Game() {
 
     console.log(`x: ${x} y: ${y}`);
 
-    // createTargetBox(x, y);
-
     return { x, y };
-  }
-
-  async function validateCoordinates(x, y) {
-    const docRef = doc(db, 'characterCoordinates', 'V6xqpoYXALe8KRYZaHTw');
-    const docSnap = await getDoc(docRef);
-    const characterCoordinates = docSnap.data();
-
-    for (const character in characterCoordinates) {
-      const correctCoordinates = characterCoordinates[character];
-
-      const validXCoordinates = [correctCoordinates[0] - 50, correctCoordinates[0] + 50];
-      const validYCoordinates = [correctCoordinates[1] - 50, correctCoordinates[1] + 50];
-      console.log(validXCoordinates, validYCoordinates);
-
-      if (
-        x > validXCoordinates[0] &&
-        x < validXCoordinates[1] &&
-        y > validYCoordinates[0] &&
-        y < validYCoordinates[1]
-      ) {
-        console.log('valid');
-        break;
-      }
-    }
   }
 
   function clickImage(event) {
@@ -97,8 +28,6 @@ export default function Game() {
     dispatch(setY(y));
 
     dispatch(clickOnImage());
-
-    validateCoordinates(x, y);
   }
 
   const imgStyle = {
--- src/components/content/game/TargetList.js
+++ src/components/content/game/TargetList.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setTargetCharacters } from '../../../redux/targetCharacters/targetCharactersSlice';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../../firebase';
 
 export default function TargetList() {
   const characters = useSelector((state) => state.characters.value);
   const targetCharacters = useSelector((state) => state.targetCharacters.value);
+  const { x, y } = useSelector((state) => state.targetCoordinates);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,10 +26,39 @@ export default function TargetList() {
     margin: '0'
   };
 
+  async function validateCoordinates(characterName) {
+    const docRef = doc(db, 'characterCoordinates', 'V6xqpoYXALe8KRYZaHTw');
+    const docSnap = await getDoc(docRef);
+    const characterCoordinates = docSnap.data();
+
+    const correctCoordinates = characterCoordinates[characterName];
+
+    const validXCoordinates = [correctCoordinates[0] - 50, correctCoordinates[0] + 50];
+    const validYCoordinates = [correctCoordinates[1] - 50, correctCoordinates[1] + 50];
+    console.log(validXCoordinates, validYCoordinates);
+
+    if (
+      x > validXCoordinates[0] &&
+      x < validXCoordinates[1] &&
+      y > validYCoordinates[0] &&
+      y < validYCoordinates[1]
+    ) {
+      console.log('valid');
+    }
+  }
+
+  function checkImage(event) {
+    const characterName = event.target.textContent.toLowerCase();
+
+    validateCoordinates(characterName);
+  }
+
   return (
     <ul style={style} className="targetList">
       {targetCharacters.map((target) => (
-        <li key={target.id}>{target.name}</li>
+        <li key={target.id} onClick={(event) => checkImage(event)}>
+          {target.name}
+        </li>
       ))}
     </ul>
   );
