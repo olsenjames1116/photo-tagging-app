32fd810c26fb3351b66e73eb6ac0f57f4e35f223
James Olsen <olsenjames1116@gmail.com>
2023-04-06 14:40:06 -0400

    Retrieve and validate coordinates from firestore
    The valid coordinates for each character are stored in firestore. The
    pixel validation is conducted on the front end to validate the user's
    selected coordinates versus the stored coordinates on the back end.

--- src/components/content/game/Game.js
+++ src/components/content/game/Game.js
@@ -4,6 +4,8 @@ import TargetBox from './TargetBox';
 import { useSelector, useDispatch } from 'react-redux';
 import { clickOnImage } from '../../../redux/imageClicked/imageClickedSlice';
 import { setX, setY } from '../../../redux/targetCoordinates/targetCoordinatesSlice';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../../firebase';
 
 export default function Game() {
   const dispatch = useDispatch();
@@ -31,37 +33,61 @@ export default function Game() {
   //   targetBox.appendChild(targetList);
   // }
 
-  function createTargetBox(x, y) {
-    // removeCurrentTargetBox();
-    // return <TargetBox x={x} y={y} />;
-    // const targetBox = document.createElement('div');
-    // targetBox.classList.add('targetBox');
-    // Object.assign(targetBox.style, {
-    //   position: 'absolute',
-    //   zIndex: '100',
-    //   top: `${y - 50}px`,
-    //   left: `${x - 50}px`,
-    //   width: '100px',
-    //   height: '100px',
-    //   border: '5px solid black'
-    // });
-    // document
-    //   .querySelector('div.content')
-    //   .insertBefore(targetBox, document.querySelector('div.content > img'));
-    // createTargetList();
-  }
+  // function createTargetBox(x, y) {
+  //   // removeCurrentTargetBox();
+  //   // return <TargetBox x={x} y={y} />;
+  //   // const targetBox = document.createElement('div');
+  //   // targetBox.classList.add('targetBox');
+  //   // Object.assign(targetBox.style, {
+  //   //   position: 'absolute',
+  //   //   zIndex: '100',
+  //   //   top: `${y - 50}px`,
+  //   //   left: `${x - 50}px`,
+  //   //   width: '100px',
+  //   //   height: '100px',
+  //   //   border: '5px solid black'
+  //   // });
+  //   // document
+  //   //   .querySelector('div.content')
+  //   //   .insertBefore(targetBox, document.querySelector('div.content > img'));
+  //   // createTargetList();
+  // }
 
   function getCoordinates(event) {
     const { left, top } = event.target.getBoundingClientRect();
     const { clientX, clientY } = event;
-    const x = clientX - left;
-    const y = clientY - top;
+    const x = Math.round(clientX - left);
+    const y = Math.round(clientY - top);
 
-    console.log(`clientX: ${clientX} clientY: ${clientY} x: ${x} y: ${y}`);
+    console.log(`x: ${x} y: ${y}`);
 
     // createTargetBox(x, y);
 
     return { x, y };
+  }
+
+  async function validateCoordinates(x, y) {
+    const docRef = doc(db, 'characterCoordinates', 'V6xqpoYXALe8KRYZaHTw');
+    const docSnap = await getDoc(docRef);
+    const characterCoordinates = docSnap.data();
+
+    for (const character in characterCoordinates) {
+      const correctCoordinates = characterCoordinates[character];
+
+      const validXCoordinates = [correctCoordinates[0] - 50, correctCoordinates[0] + 50];
+      const validYCoordinates = [correctCoordinates[1] - 50, correctCoordinates[1] + 50];
+      console.log(validXCoordinates, validYCoordinates);
+
+      if (
+        x > validXCoordinates[0] &&
+        x < validXCoordinates[1] &&
+        y > validYCoordinates[0] &&
+        y < validYCoordinates[1]
+      ) {
+        console.log('valid');
+        break;
+      }
+    }
   }
 
   function clickImage(event) {
@@ -71,6 +97,8 @@ export default function Game() {
     dispatch(setY(y));
 
     dispatch(clickOnImage());
+
+    validateCoordinates(x, y);
   }
 
   const imgStyle = {
