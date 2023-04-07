be26eef03a088d991d39445afde4b2c337172b05
James Olsen <olsenjames1116@gmail.com>
2023-04-07 15:12:54 -0400

    Create high scores components
    Components to display the players' high scores were created. These will
    access firestore in the future to get scores for the users.

---
+++ src/components/content/exit/HighScores.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PlayerHighScores from './PlayerHighScores';
+
+export default function HighScores() {
+  return (
+    <div className="highScores">
+      <h2>High Scores:</h2>
+      <PlayerHighScores />
+    </div>
+  );
+}
--- src/components/content/exit/Scores.js
+++ src/components/content/exit/Scores.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PlayerTime from './PlayerTime';
 import PlayerForm from './PlayerForm';
+import HighScores from './HighScores';
 
 export default function Scores() {
   return (
     <div className="scores">
       <PlayerTime />
       <PlayerForm />
+      <HighScores />
     </div>
   );
 }
