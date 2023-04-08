dadda37c33566c6a2ded40304555a4157d10d44a
James Olsen <olsenjames1116@gmail.com>
2023-04-08 17:29:45 -0400

    Validate name input from player
    Ensure a user has entered a name to store their score in firestore.

--- src/components/content/exit/PlayerForm.js
+++ src/components/content/exit/PlayerForm.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../../../firebase';
+import { useSelector } from 'react-redux';
 
 export default function PlayerForm() {
+  const stopwatch = useSelector((state) => state.stopwatch.value);
+  const nameInput = document.querySelector('input#playerName');
+
+  async function writeDocument(playerName) {
+    const docRef = await addDoc(collection(db, 'highScores'), {
+      name: playerName,
+      time: stopwatch
+    });
+  }
+
+  function displayErrorMessage(nameInput) {
+    console.log('invalid submission');
+
+    if (nameInput.validity.valueMissing) {
+      nameInput.setCustomValidity('Please enter a name');
+    }
+
+    nameInput.reportValidity();
+  }
+
+  function checkValidity(event) {
+    event.preventDefault();
+
+    const form = document.querySelector('form');
+
+    if (form.checkValidity()) {
+      const playerName = nameInput.value;
+      console.log(playerName, stopwatch);
+      // writeDocument(playerName);
+    } else {
+      displayErrorMessage(nameInput);
+    }
+  }
+
+  function resetValidity() {
+    nameInput.setCustomValidity('');
+  }
+
   return (
-    <form>
+    <form noValidate>
       <label htmlFor="playerName">Enter your name:</label>
-      <input type="text" placeholder="Peter Parker" />
-      <button type="submit" onClick={(event) => event.preventDefault()}>
+      <input
+        id="playerName"
+        type="text"
+        placeholder="Thor"
+        required
+        maxLength="20"
+        onChange={() => resetValidity()}
+      />
+      <button type="submit" onClick={(event) => checkValidity(event)}>
         Submit
       </button>
     </form>
