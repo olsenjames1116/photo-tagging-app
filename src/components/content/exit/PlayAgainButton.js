a08cba3fa3b0dcc5b99acbeb25306a5751174dc4
James Olsen <olsenjames1116@gmail.com>
2023-04-08 15:30:20 -0400

    Play again button reloads page when clicked

--- src/components/content/exit/PlayAgainButton.js
+++ src/components/content/exit/PlayAgainButton.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function PlayAgainButton() {
-  return <button type="button">Play Again</button>;
+  return (
+    <button type="button" onClick={() => location.reload()}>
+      Play Again
+    </button>
+  );
 }
