1bc2de765324df3cdcd924d687de44e97b118149
James Olsen <olsenjames1116@gmail.com>
2023-04-10 18:07:08 -0400

    Add author and source credits in footer

--- src/components/footer/Sources.js
+++ src/components/footer/Sources.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function Sources() {
-  return <p className="sources">Images provided by...</p>;
+  return (
+    <p className="sources">
+      Images provided by <a href="https://www.pngegg.com/">PNG EGG</a>,{' '}
+      <a href="https://toppng.com/">TopPNG</a>, <a href="https://www.pngwing.com/">PNGWing</a> and{' '}
+      <a href="https://toddnauck.com/">Todd Nauck</a>
+    </p>
+  );
 }
